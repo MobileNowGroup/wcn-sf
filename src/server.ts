0872ea828c126874bf9d811538434a9e99d8e2c3
vincent.wen <wenwei1202@gmail.com>
2018-02-23 15:30:24 +0800

    add send message api

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ app.use(serve(__dirname + '/client'))
 
 const router = new Router()
 
-router.get('/api/messages', (ctx, next) => {
+router.get('/api/messages', async (ctx, next) => {
   const messages = {
     'Welcome Message': 'Welcome to the testing of SF and WCN!',
     'Thank You Message': "Thank you for your order. We'll process your order soon.",
@@ -17,6 +17,16 @@ router.get('/api/messages', (ctx, next) => {
       'Congratulations! You membership card is upgraded to PLATINUM! Check out the benefits now!'
   }
   ctx.body = messages
+})
+
+router.get('/api/sendmessage', async (ctx, next) => {
+  const openId = ctx.query.openId
+  if(openId === 'myOpenId') {
+    ctx.body = 'SUCCESS'
+  } else {
+    ctx.status = 400
+    ctx.body = 'Invliad open ID.'
+  }
 })
 
 app.use(router.routes()).use(router.allowedMethods())
